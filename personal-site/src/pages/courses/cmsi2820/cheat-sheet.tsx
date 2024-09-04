b2e710ed95d07c9d51e56b2b4902ceba817b1e36
Jgonz156 <Jgonz156@lion.lmu.edu>
2024-09-04 16:42:36 -0700

    added useful commands to cheat sheet

--- personal-site/src/pages/courses/cmsi2820/cheat-sheet.tsx
+++ personal-site/src/pages/courses/cmsi2820/cheat-sheet.tsx
@@ -2,6 +2,7 @@ import { MathJax } from "better-react-mathjax"
 import CourseBox from "../components/course-box"
 import CoursePage from "../components/course-page"
 import Speak from "../components/speak"
+import TopicBreak from "../components/topic-break"
 
 export default function CheatSheet() {
   return (
@@ -16,6 +17,24 @@ export default function CheatSheet() {
         </Speak>
       </CourseBox>
       <CourseBox>
+        <TopicBreak title="Useful Commands" />
+        <MathJax>{`$$\\begin{matrix}
+        \\text{Terminal Command} & \\text{Example} \\\\
+        \\text{pwd} & \\text{>>>pwd} \\\\
+        \\text{cd P} & \\text{>>>cd homework/cmsi-2820-hw0/} \\\\
+        \\text{ls} & \\text{>>>ls} \\\\
+        \\text{mv P1 P2} & \\text{>>>mv photos/orangecat.jpg cats/orange} \\\\
+        \\text{git status} & \\text{>>>git status} \\\\
+        \\text{git add P} & \\text{>>>git add .} \\\\
+        \\text{git commit -m "<Commit Message>"} & \\text{>>>git commit -m "Completed HW"} \\\\
+        \\text{git push} & \\text{>>>git push} \\\\
+        \\text{git pull} & \\text{>>>git pull} \\\\
+        \\text{python F.py} & \\text{>>>python questions.py} \\\\
+        \\text{python -m pytest} & \\text{>>>python -m pytest} \\\\
+        \\end{matrix}$$`}</MathJax>
+      </CourseBox>
+      <CourseBox>
+        <TopicBreak title="Logic" />
         <MathJax>{`\\[\\top:Reality, \\bot:Absurdity, T:True, F:False\\]`}</MathJax>
         <MathJax>{`$$\\text{Propositional Variables: }X_{0},X_{1},X_{2},...$$`}</MathJax>
         <MathJax>{`$$\\begin{matrix}
