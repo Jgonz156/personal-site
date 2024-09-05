066a655b47b7ef1b9950f49ddadeb4c2b50d4861
Jgonz156 <Jgonz156@lion.lmu.edu>
2024-09-04 17:04:45 -0700

    forced english style calendar in the localization provider

--- personal-site/src/main.tsx
+++ personal-site/src/main.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import App from "./pages/App.tsx";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import App from "./pages/App.tsx"
 import {
   experimental_extendTheme as materialExtendTheme,
   Experimental_CssVarsProvider as MaterialCssVarsProvider,
   THEME_ID as MATERIAL_THEME_ID,
-} from "@mui/material/styles";
+} from "@mui/material/styles"
 import {
   CssVarsProvider as JoyCssVarsProvider,
   extendTheme as joyExtendTheme,
-} from "@mui/joy";
-import CMSI2820 from "./pages/courses/cmsi2820/cmsi-2820-discrete-mathematics-for-cs.tsx";
-import { SiteContextProvider } from "./components/site-context.tsx";
-import "./index.css";
-import CheatSheet from "./pages/courses/cmsi2820/cheat-sheet.tsx";
-import Syllabus from "./pages/courses/cmsi2820/syllabus.tsx";
-import LectureNotes0 from "./pages/courses/cmsi2820/notes/ln0.tsx";
-import LectureNotes1 from "./pages/courses/cmsi2820/notes/ln1.tsx";
-import LectureNotes2 from "./pages/courses/cmsi2820/notes/ln2.tsx";
-import LectureNotes3 from "./pages/courses/cmsi2820/notes/ln3.tsx";
-import HW0 from "./pages/courses/cmsi2820/assignments/hw0.tsx";
-import HW1 from "./pages/courses/cmsi2820/assignments/hw1.tsx";
-import HW2 from "./pages/courses/cmsi2820/assignments/hw2.tsx";
-import HW3 from "./pages/courses/cmsi2820/assignments/hw3.tsx";
-import HW4 from "./pages/courses/cmsi2820/assignments/hw4.tsx";
-import HW5 from "./pages/courses/cmsi2820/assignments/hw5.tsx";
-import HW6 from "./pages/courses/cmsi2820/assignments/hw6.tsx";
-import HW7 from "./pages/courses/cmsi2820/assignments/hw7.tsx";
-import EX0 from "./pages/courses/cmsi2820/exams/ex0.tsx";
-import EX1 from "./pages/courses/cmsi2820/exams/ex1.tsx";
-import EX2 from "./pages/courses/cmsi2820/exams/ex2.tsx";
-import { MathJaxContext } from "better-react-mathjax";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
+} from "@mui/joy"
+import CMSI2820 from "./pages/courses/cmsi2820/cmsi-2820-discrete-mathematics-for-cs.tsx"
+import { SiteContextProvider } from "./components/site-context.tsx"
+import "./index.css"
+import CheatSheet from "./pages/courses/cmsi2820/cheat-sheet.tsx"
+import Syllabus from "./pages/courses/cmsi2820/syllabus.tsx"
+import LectureNotes0 from "./pages/courses/cmsi2820/notes/ln0.tsx"
+import LectureNotes1 from "./pages/courses/cmsi2820/notes/ln1.tsx"
+import LectureNotes2 from "./pages/courses/cmsi2820/notes/ln2.tsx"
+import LectureNotes3 from "./pages/courses/cmsi2820/notes/ln3.tsx"
+import HW0 from "./pages/courses/cmsi2820/assignments/hw0.tsx"
+import HW1 from "./pages/courses/cmsi2820/assignments/hw1.tsx"
+import HW2 from "./pages/courses/cmsi2820/assignments/hw2.tsx"
+import HW3 from "./pages/courses/cmsi2820/assignments/hw3.tsx"
+import HW4 from "./pages/courses/cmsi2820/assignments/hw4.tsx"
+import HW5 from "./pages/courses/cmsi2820/assignments/hw5.tsx"
+import HW6 from "./pages/courses/cmsi2820/assignments/hw6.tsx"
+import HW7 from "./pages/courses/cmsi2820/assignments/hw7.tsx"
+import EX0 from "./pages/courses/cmsi2820/exams/ex0.tsx"
+import EX1 from "./pages/courses/cmsi2820/exams/ex1.tsx"
+import EX2 from "./pages/courses/cmsi2820/exams/ex2.tsx"
+import { MathJaxContext } from "better-react-mathjax"
+import { LocalizationProvider } from "@mui/x-date-pickers"
+import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon"
 
 const router = [
   { path: "/", element: <App /> },
@@ -55,16 +55,16 @@ const router = [
   { path: "/cmsi-2820/ex0", element: <EX0 /> },
   { path: "/cmsi-2820/ex1", element: <EX1 /> },
   { path: "/cmsi-2820/ex2", element: <EX2 /> },
-];
+]
 
-const materialTheme = materialExtendTheme();
+const materialTheme = materialExtendTheme()
 
 const joyTheme = joyExtendTheme({
   colorSchemes: {
     light: { palette: { warning: { 500: "#c46d0a" } } },
     dark: { palette: { warning: { 500: "#c46d0a" } } },
   },
-});
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -77,10 +77,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <JoyCssVarsProvider theme={joyTheme} defaultMode="dark">
             <LocalizationProvider
               dateAdapter={AdapterLuxon}
+              adapterLocale="en"
               localeText={{
                 calendarWeekNumberHeaderText: "School Week",
                 calendarWeekNumberText: (weekNumber) => {
-                  const weekOffset = (weekNumber + 34) % 34;
+                  const weekOffset = (weekNumber + 34) % 34
                   return weekOffset <= 0 || weekOffset > 16
                     ? ""
                     : weekOffset % 10 === 1 && weekOffset !== 11
@@ -89,7 +90,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                     ? `${weekOffset}nd`
                     : weekOffset % 10 === 3 && weekOffset !== 13
                     ? `${weekOffset}rd`
-                    : `${weekOffset}th`;
+                    : `${weekOffset}th`
                 },
               }}
             >
@@ -106,4 +107,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       </MathJaxContext>
     </SiteContextProvider>
   </React.StrictMode>
-);
+)
