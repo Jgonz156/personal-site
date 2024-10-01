ba435536c7843b9dac4d4a43cc02794659016c5e
jgonz156 <julian.gonzalez@lmue.edu>
2024-10-01 02:23:13 -0700

    altered imports on ln10

--- personal-site/src/pages/courses/cmsi2820/notes/ln10.tsx
+++ personal-site/src/pages/courses/cmsi2820/notes/ln10.tsx
@@ -1,8 +1,5 @@
-import { MathJax } from "better-react-mathjax"
 import CourseBox from "../../components/course-box"
 import CoursePage from "../../components/course-page"
-import Speak from "../../components/speak"
-import PhilosophyBreak from "../../components/philosophy-break"
 import TitleBox from "../../components/title-box"
 
 export default function LectureNotes9() {
