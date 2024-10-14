189f2d135b1f5f279405d34cd27c069391af7379
Jgonz156 <Jgonz156@lion.lmu.edu>
2024-10-14 14:48:40 -0700

    added that the hours are in the keck lab

--- personal-site/src/components/footer.tsx
+++ personal-site/src/components/footer.tsx
@@ -33,7 +33,8 @@ const personalInfo = [
     icon: <MeetingRoomIcon />,
   },
   {
-    value: "Office Hours: M 5PM-7PM , W 2PM-5PM, F 5PM-7PM",
+    value:
+      "Office Hours: M 5PM-7PM (Keck Lab), W 2PM-5PM, F 5PM-7PM (Keck Lab)",
     icon: <AccessTimeIcon />,
   },
   {
